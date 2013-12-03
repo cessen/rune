5d1c73e7542b352930ef62838b1ec3e474b5bd39
Nathan Vegdahl <cessen@cessen.com>
2013-12-02 22:35:20 -0800

    Beginnings of a basic lexer.
    Cool thing: it already handles utf8 properly.

--- lexer/lexer.hpp
+++ lexer/lexer.hpp
@@ -1,10 +1,41 @@
 #ifndef LEXER_HPP
 #define LEXER_HPP
 
+#include <utility>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+enum TokenType {
+    IDENTIFIER,
+    OPERATOR,
+    PAREN_OPEN,
+    PAREN_CLOSE
+};
+
+std::string next_utf8(std::istream &in);
+
 class Lexer
 {
 public:
+	// TODO: change to use move semantics once gcc implements them for streams.
+	// This current implementation of the constructor is delicate
+	Lexer(std::istream* input): in {*input} {
+		cur_c = next_utf8(in);
+	}
+
+	std::string next();
+
+	//std::string token();
+
+	//TokenType token_type();
+
+
 private:
+	std::istream& in;
+	std::string cur_c;
+	std::string current_token;
+	TokenType current_token_type;
 };
 
 #endif // DATASTORE_HPP
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "config.h"
 
+#include <fstream>
 #include <iostream>
 
 #include "lexer.hpp"
@@ -7,6 +8,23 @@
 int main(int argc, char** argv)
 {
 	std::cout << "NLang v" << VERSION_MAJOR << "." << VERSION_MINOR << "." << VERSION_PATCH << "\n";
+
+	if (argc < 2) {
+		std::cout << "You must specify a file to compile.\n";
+		return 0;
+	}
+
+	std::ifstream f {argv[1]};
+
+	Lexer l {&f};
+
+	std::string s;
+
+	s = l.next();
+	while (s != std::string("")) {
+		std::cout << s << std::endl;
+		s = l.next();
+	}
 
 	return 0;
 }
