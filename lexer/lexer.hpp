e1af4955506cd9b339b883cb95bcb81ed4262902
Nathan Vegdahl <cessen@cessen.com>
2013-12-02 19:39:37 -0800

    First commit.  NLang is working title.  Will almost certainly change.

---
+++ lexer/lexer.hpp
@@ -0,0 +1,10 @@
+#ifndef LEXER_HPP
+#define LEXER_HPP
+
+class Lexer
+{
+public:
+private:
+};
+
+#endif // DATASTORE_HPP
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "config.h"
+
+#include <iostream>
+
+#include "lexer.hpp"
+
+int main(int argc, char** argv)
+{
+	std::cout << "NLang v" << VERSION_MAJOR << "." << VERSION_MINOR << "." << VERSION_PATCH << "\n";
+
+	return 0;
+}
