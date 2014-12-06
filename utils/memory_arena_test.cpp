c29ce9fc4e4ae3fa9a531364d6b02c47845402b9
Nathan Vegdahl <cessen@cessen.com>
2014-12-05 22:40:38 -0800

    Added more unit tests for MemoryArena.

--- utils/memory_arena_test.cpp
+++ utils/memory_arena_test.cpp
@@ -1,6 +1,10 @@
 #include "catch.hpp"
 
 #include <cstdint>
+#include <vector>
+#include <list>
+#include "slice.hpp"
+
 #include "memory_arena.hpp"
 
 
@@ -25,6 +29,61 @@ TEST_CASE("Sequential memory addresses", "[memory_arena]")
 	REQUIRE((a+1) == b);
 	REQUIRE((a+2) == c);
 	REQUIRE((a+3) == d);
+}
+
+
+// Make sure alloc() initializes things properly if given a value
+TEST_CASE("alloc() init", "[memory_arena]")
+{
+	MemoryArena<> arena;
+
+	int32_t* a = arena.alloc<int32_t>(42);
+	int32_t* b = arena.alloc<int32_t>(64);
+
+	REQUIRE((*a) == 42);
+	REQUIRE((*b) == 64);
+}
+
+
+// Make sure alloc_array() creates a slice of the appropriate length
+TEST_CASE("alloc_from_array() length", "[memory_arena]")
+{
+	MemoryArena<> arena;
+
+	Slice<int32_t> s = arena.alloc_array<int32_t>(123);
+
+	REQUIRE(s.size() == 123);
+}
+
+
+// Make sure alloc_from_iters() initializes things properly
+TEST_CASE("alloc_from_iters() init", "[memory_arena]")
+{
+	MemoryArena<64> arena;
+
+	std::vector<int32_t> v {1, 0, 2, 9, 3, 8, 4, 7, 5, 6};
+	std::list<int32_t> l {1, 0, 2, 9, 3, 8, 4, 7, 5, 6};
+
+	Slice<int32_t> s1 = arena.alloc_from_iters(v.begin(), v.end());
+	Slice<int32_t> s2 = arena.alloc_from_iters(l.begin(), l.end());
+
+	REQUIRE(v.size() == s1.size());
+	REQUIRE(l.size() == s2.size());
+
+	auto i1 = v.begin();
+	auto i2 = l.begin();
+	auto i3 = s1.begin();
+	auto i4 = s2.begin();
+
+	for (size_t i = 0; i < s1.size(); ++i) {
+		REQUIRE((*i1) == (*i3));
+		REQUIRE((*i2) == (*i4));
+
+		++i1;
+		++i2;
+		++i3;
+		++i4;
+	}
 }
 
 
